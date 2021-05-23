5f6effc560033851f07deea8ae7e630f0af2eb36
gitiklar <65737909+gitiklar@users.noreply.github.com>
2021-05-23 13:12:20 +0300

    update

--- client/src/service.js
+++ client/src/service.js
@@ -8,12 +8,9 @@ const doFetch = async (url , method , sendToken , body = null , contentType=null
     const options = {
         method: method,
         mode: 'cors',
-        headers: {},
+        headers: { 'Content-Type' : 'application/json'},
     };
-    !contentType && (options.headers['Content-Type'] = 'application/json');
-    contentType && (options.headers['Content-Type'] = contentType);
-    body && !contentType && (options.body = JSON.stringify(body));
-    body && contentType && (options.body = body);
+    body && (options.body = JSON.stringify(body));
     sendToken && (options.headers['x-access-token'] = getToken());
     const response = await fetch(url , options);
     if(response.headers.get('Content-Type').startsWith('application/json')) {
@@ -22,8 +19,8 @@ const doFetch = async (url , method , sendToken , body = null , contentType=null
     return response;
 }
 
-export const postRequest = async (url , sendToken , data , contentType) => {
-    return await doFetch(`${serverUrl}${url}` , 'POST' , sendToken , data , contentType);
+export const postRequest = async (url , sendToken , data) => {
+    return await doFetch(`${serverUrl}${url}` , 'POST' , sendToken , data);
 }
 
 export const getRequest = async (url , sendToken , data) => {
@@ -45,23 +42,3 @@ api.interceptors.request.use(function (config) {
 });
 
 export const addCandyToDB = (candy) => api.post('/candy', candy, {'Content-Type': 'multipart/form-data'}).then(response => response.data);
-
-/*
-
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
-})
-
-api.interceptors.request.use(function (config) {
-    const token = localStorage.getItem('token');
-    config.headers.Authorization =  token ? `Bearer ${token}` : '';
-    return config;
-});
-
-export const addCandy = (candy) => api.post('/candy', candy, {'Content-Type': 'multipart/form-data'}).then(response => response.data);
-export const bulkUpdateAmount = (candies) => api.post('/candy/bulk', candies).then(response => response.data);
-export const updateCandy = (candy) => api.put(`/candy/${candy.key}`, candy).then(response => response.data);
-export const getAllCandies = () => api.get('/candies',{'Authorization' : null}).then(response => response.data);
-*/
